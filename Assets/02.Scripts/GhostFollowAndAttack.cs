6c3544dae9e46a02704796f6412f271ffdbd88a7
JlNUTV <qkrwlsdn0623@yonsei.ac.kr>
2025-06-04 05:20:56 +0900

    그릇음식 안먹는 버그 수정

--- Assets/02.Scripts/GhostFollowAndAttack.cs
+++ Assets/02.Scripts/GhostFollowAndAttack.cs
@@ -50,6 +50,13 @@ public class GhostFollowAndAttack : MonoBehaviour
             if (animator == null)
                 Debug.LogWarning("Animator 컴포넌트를 찾을 수 없습니다. 프리팹에 Animator 붙여주세요.");
         }
+
+        // GhostPreferenceSystem 참조
+        preferenceSystem = GetComponent<GhostPreferenceSystem>();
+        if (preferenceSystem == null)
+        {
+            Debug.LogWarning("GhostPreferenceSystem 컴포넌트를 찾을 수 없습니다!");
+        }
     }
 
     void Update()
